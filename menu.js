725dd6a8e50292749d5dc0b536793b0a7b712122
Angel1485 <andyjp_666@hotmail.com>
2026-04-14 20:13:02 -0500

    CSS y JS realizado por la IA

---
+++ menu.js
@@ -0,0 +1,8 @@
+function abrirJuego(juego) {
+    if (juego === "cazando") {
+        window.location.href = "https://juego-cazando-taupe.vercel.app/";
+    } 
+    else if (juego === "limones") {
+        window.location.href = "https://juego-limones-beta.vercel.app/";
+    }
+}
