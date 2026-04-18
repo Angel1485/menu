bd0a4d3d514c7c29d199c95eadea54798ea84dd8
Angel1485 <andyjp_666@hotmail.com>
2026-04-18 16:36:24 -0500

    Agregacion de comisiones

--- menu.js
+++ menu.js
@@ -1,14 +1,15 @@
 function abrirJuego(juego) {
     if (juego === "cazando") {
         window.location.href = "https://juego-cazando-taupe.vercel.app/";
-    } 
-    else if (juego === "limones") {
+    } else if (juego === "limones") {
         window.location.href = "https://juego-limones-beta.vercel.app/";
     }
 }
 
-function simuladorCredito(id) {
+function simuladores(id) {
     if (id === "simulador") {
         window.location.href = "https://simulador-de-creditos-mu.vercel.app/";
-    } 
+    } else if(id === "comisiones"){
+        window.location.href = "https://comisiones-delta.vercel.app/";
+    }
 }
