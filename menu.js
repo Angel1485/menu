8fbca4eb4305179be73f536b44affd90693cc17d
Angel1485 <andyjp_666@hotmail.com>
2026-05-05 21:55:39 -0500

    menu actualizado

--- menu.js
+++ menu.js
@@ -11,11 +11,19 @@ function simuladores(id) {
         window.location.href = "https://simulador-de-creditos-mu.vercel.app/";
     } else if(id === "comisiones"){
         window.location.href = "https://comisiones-delta.vercel.app/";
+    }else if(id === "simulador2Arreglos"){
+        window.location.href = "https://progra-js-movilis2-cbet.vercel.app/";
     }
 }
 
 function multiplicacion(id) {
     if (id === "tablas") {
         window.location.href = "https://progra-js-movilis2.vercel.app/";
+    } 
+}
+
+function arreglos(id) {
+    if (id === "edades") {
+        window.location.href = "https://progra-js-movilis2-6wck.vercel.app/";
     } 
 }
